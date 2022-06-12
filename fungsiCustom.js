c026b8f594e80449bfaad2470d092b3e3852e425
Hendri Sibarani <hendrisibarani15@gmail.com>
2022-06-12 17:55:33 +0700

    done

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,51 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const getSecondWord = dataObject => {
+  const parsedData = JSON.parse(dataObject);
+  
+  if (parsedData.message != undefined) {
+      const word = parsedData.message.split(" ");
+      return word[1];
+  } else if (parsedData[0].message != undefined) {
+      const word = parsedData[0].message.split(" ");
+      return word[1];
+  } else if (parsedData[0].data.message != undefined) {
+      const word = parsedData[0].data.message.split(" ");
+      return word[1];
+  }
+};
+
+const bacaData = (fnCallback) => {
+  const result = [];
+
+  fs.readFile(file1, 'utf8', (error, dataYangDibaca) => {
+      if (error) error;
+      const secondWord = getSecondWord(dataYangDibaca);
+      result.push(secondWord);
+
+      fs.readFile(file2, 'utf8', (error, dataYangDibaca) => {
+          if (error) error;
+          const secondWord = getSecondWord(dataYangDibaca);
+          result.push(secondWord);
+  
+          fs.readFile(file3, 'utf8', (error, dataYangDibaca) => {
+              if (error) error;
+              const secondWord = getSecondWord(dataYangDibaca);
+              result.push(secondWord);
+      
+              fnCallback(null, result);        
+          });
+      });
+  });
+};
+
+function fnCallback(file1, file2, file3) {
+  let arrFile = [file1, file2, file3];
+  for (let e of arrFile){
+    return e;
+  }
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
